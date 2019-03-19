b2dea620107f9aca0ccd866acc4d296fe328343a
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-03-19 15:57:26 +0000

    expanded testing

--- tests/test_general.py
+++ tests/test_general.py
@@ -5,3 +5,25 @@ def test_file():
     with URLZSource('tests/general.txt').open() as source:
         content = source.readlines()
         assert content == ["Foo"]
+        
+def test_file_gz():
+    with URLZSource('tests/general.txt.gz').open() as source:
+        content = source.readlines()
+        assert content == ["Foo"]
+
+def test_file_gz():
+    with URLZSource('tests/general.txt.zip').open() as source:
+        content = source.readlines()
+        assert content == ["Foo"]
+
+def test_url():
+    url = "https://raw.githubusercontent.com/opentargets/urlzsource/master/tests/general.txt"
+    with URLZSource(url).open() as source:
+        content = source.readlines()
+        assert content == ["Foo"]
+
+def test_file_url():
+    with URLZSource('file://./tests/general.txt').open() as source:
+        content = source.readlines()
+        assert content == ["Foo"]
+        

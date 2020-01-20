1f2807036193477fab10ee50bd09ec69117222ae
Fedde Schaeffer <fedde@thehyve.nl>
2020-01-20 11:29:12 +0100

    Fix linter errors in test file
    One of these caused a test to be quietly ignored; it should have pointed
    out a bug in the code.

--- tests/test_general.py
+++ tests/test_general.py
@@ -5,16 +5,19 @@ def test_file():
     with URLZSource('tests/general.txt').open() as source:
         content = source.readlines()
         assert content == ["Foo"]
-        
+
+
 def test_file_gz():
     with URLZSource('tests/general.txt.gz').open() as source:
         content = source.readlines()
         assert content == ["Foo"]
 
-def test_file_gz():
+
+def test_file_zip():
     with URLZSource('tests/general.txt.zip').open() as source:
         content = source.readlines()
         assert content == ["Foo"]
+
 
 def test_url():
     url = "https://raw.githubusercontent.com/opentargets/urlzsource/master/tests/general.txt"
@@ -22,8 +25,8 @@ def test_url():
         content = source.readlines()
         assert content == ["Foo"]
 
+
 def test_file_url():
     with URLZSource('file://./tests/general.txt').open() as source:
         content = source.readlines()
         assert content == ["Foo"]
-        
